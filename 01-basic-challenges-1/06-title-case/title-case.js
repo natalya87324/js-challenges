09c7ec97ecc6be56ed29a8ab62f78689eca6a59f
Natalya <kim87324@yandex.ru>
2024-01-12 23:42:17 +0300

    My solution of title-case challenge

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,18 @@
-function titleCase() {}
+function titleCase(str) {
+  const stringArr = str.split(' ');
+  
+  const finalString = [];
+
+  for (let i = 0; i < stringArr.length; i++) {
+    let word = stringArr[i];
+    word = word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase();
+
+    finalString.push(word);
+    
+  }
+ 
+  return finalString.join(" ");
+  
+}
 
 module.exports = titleCase;
