a54259ed88733b9a4e04601d9113aba5960897a3
Natalya <kim87324@yandex.ru>
2024-02-24 22:51:06 +0300

    Array intersection challenge solution

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,13 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const resultArray = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i])) {
+      resultArray.push(arr1[i])
+    }
+  }
+
+  return resultArray;
+}
 
 module.exports = arrayIntersection;
