ddaa8632a3880b9f9045634db55387fba9c494fa
Natalya <kim87324@yandex.ru>
2024-02-24 23:42:29 +0300

    Add verification that the element is not already in the result array

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -2,7 +2,7 @@ function arrayIntersection(arr1, arr2) {
   const resultArray = [];
 
   for (let i = 0; i < arr1.length; i++) {
-    if (arr2.includes(arr1[i])) {
+    if (arr2.includes(arr1[i]) && !resultArray.includes(arr1[i])) {
       resultArray.push(arr1[i])
     }
   }
