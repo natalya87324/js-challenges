e50c0defe005646b82e287022ef2c65e04c0488a
Natalya <kim87324@yandex.ru>
2024-02-28 15:40:58 +0300

    Find miising number challenge solution using for loop

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,20 @@
-function findMissingNumber() {}
+function findMissingNumber(nums) {
+  let n = nums.length + 1;
+  let expectedSum = n * (n + 1) / 2;
+  let actualSum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    actualSum += nums[i];
+  }
+
+  if (nums.length === 0) {
+    return 1;
+  } else if (!nums) {
+    return undefined;
+  } else {
+    return expectedSum - actualSum;
+  }
+
+}
 
 module.exports = findMissingNumber;
