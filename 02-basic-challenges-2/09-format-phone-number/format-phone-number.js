7fda425c02ea34cb2d10bd7e7cc351275eeb2ff8
Natalya <kim87324@yandex.ru>
2024-03-20 21:57:42 +0300

    My first raw solution of the challenge

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,13 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(arr) {
+  let numString;
+
+  const numGroup1 = arr.slice(0, 3).join('');
+  const numGroup2 = arr.slice(3, 6).join('');
+  const numGroup3 = arr.slice(6).join('');
+
+  numString = `(${numGroup1}) ${numGroup2}-${numGroup3}`;
+
+  return numString;
+}
 
 module.exports = formatPhoneNumber;
