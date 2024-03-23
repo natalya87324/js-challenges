ec32cf89b21228acd04b78e414898a3d4e48d495
Natalya <kim87324@yandex.ru>
2024-03-23 23:49:46 +0300

    Challenge solution 1

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,8 @@
-function validateEmail() {}
+function validateEmail(email) {
+  const emailRegExp =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  return emailRegExp.test(String(email).toLowerCase());
+}
 
 module.exports = validateEmail;
