14f1764ac9c601e4a02518d4d4a8e1274de99370
Natalya <kim87324@yandex.ru>
2024-03-25 22:22:19 +0300

    Challenge solution before refactoring

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,11 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  const evenNums = numbers.filter((num) => num % 2 === 0);
+  const squareNums = evenNums.map((num) => num ** 2);
+  const finalSum = squareNums.reduce((total, value) => {
+    return total + value;
+  }, 0);
+
+  return finalSum;
+}
 
 module.exports = sumOfEvenSquares;
