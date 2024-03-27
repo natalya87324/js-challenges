e2fcf7ea14e8868813975b08e837b8b0830ed1cf
Natalya <kim87324@yandex.ru>
2024-03-27 14:18:29 +0300

    Challenge solution version 1

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,19 @@
-function highestScoringWord() {}
+function highestScoringWord(wordsString) {
+  const wordsArr = wordsString.toLowerCase().split(' ');
+
+  let score = 0;
+  let highestWord = '';
+  let currentScore = 0;
+  wordsArr.map((word) => {
+    currentScore = word.split('').reduce((sum, letter) => {
+      return sum + letter.charCodeAt(0) - 96;
+    }, 0);
+    if (currentScore > score) {
+      score = currentScore;
+      highestWord = word;
+    }
+  });
+  return highestWord;
+}
 
 module.exports = highestScoringWord;
