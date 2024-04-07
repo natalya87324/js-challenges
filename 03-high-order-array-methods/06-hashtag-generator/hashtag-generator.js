4627f0eab212fa187781531397d6f2cf6887591c
Natalya <kim87324@yandex.ru>
2024-04-07 21:22:25 +0300

    Challenge solution 1

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,17 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+  if (str.trim() === '') {
+    return false;
+  }
+
+  const words = str.trim().split(/\s+/);
+
+  const capitalizedWords = words.map(
+    (word) => word.charAt(0).toUpperCase() + word.substring(1)
+  );
+
+  const hashtag = '#' + capitalizedWords.join('');
+
+  return hashtag.length > 140 ? false : hashtag;
+}
 
 module.exports = generateHashtag;
