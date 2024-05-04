ecb653548c5f261f268cf4816015886b898e64c9
Natalya <kim87324@yandex.ru>
2024-05-04 22:50:59 +0300

    Challenge solution

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,24 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+  const totalMileage = cars.reduce((total, car) => total + car.mileage, 0);
+
+  const averageMileage = totalMileage / cars.length;
+
+  const highestMileageCar = cars.reduce(
+    (highest, car) => (car.mileage > highest.mileage ? car : highest),
+    cars[0]
+  );
+
+  const lowestMileageCar = cars.reduce(
+    (lowest, car) => (car.mileage < lowest.mileage ? car : lowest),
+    cars[0]
+  );
+
+  return {
+    averageMileage: parseFloat(averageMileage.toFixed(2)),
+    highestMileageCar,
+    lowestMileageCar,
+    totalMileage,
+  };
+}
 
 module.exports = analyzeCarMileage;
