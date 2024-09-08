ea57929698914885c0e3ba955903c47be9c71468
Natalya <kim87324@yandex.ru>
2024-09-08 14:46:11 +0300

    my solution reverse-string-recursion

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,11 @@
-function reverseString() {}
+function reverseString(str) {
+  if (str === '') {
+    return '';
+  }
+
+  reverseString(str.slice(1));
+
+  return reverseString(str.slice(1)) + str[0];
+}
 
 module.exports = reverseString;
