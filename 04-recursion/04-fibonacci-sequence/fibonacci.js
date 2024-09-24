6119e12fab1d2abcb609eafb4b2359a9753b6a26
Natalya <kim87324@yandex.ru>
2024-09-24 23:40:46 +0300

    add solution to fibonacci challenge

--- 04-recursion/04-fibonacci-sequence/fibonacci.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci.js
@@ -1,3 +1,10 @@
-function fibonacci() {}
+function fibonacci(num) {
+  //Base case. Parameter num represents the spot (index) in the fibonacci sequence
+  if (num < 2) {
+    return num;
+  }
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
 
 module.exports = fibonacci;
